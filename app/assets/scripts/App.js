2b8c827cef4b7d3d89ffa6247a4185fb9a2cbc38
Mosaab <mosaab_saleh@hotmail.com>
2019-01-12 23:27:43 +0000

    finishing header styles

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,3 @@
+import MobileMenu from './modules/MobileMenu';
+
+var mbileMenu = new MobileMenu();
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
